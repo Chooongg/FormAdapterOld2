cd9aa0d818b00b1a5ac19f491d0c5aec30a57030
Chooongg <chooongg@outlook.com>
2023-06-16 18:01:49 +0800

    .

--- formAdapter/src/main/java/com/chooongg/formAdapter/item/SingleLineForm.kt
+++ formAdapter/src/main/java/com/chooongg/formAdapter/item/SingleLineForm.kt
@@ -1,13 +1,9 @@
 package com.chooongg.formAdapter.item
 
-import androidx.annotation.IntRange
 import com.chooongg.formAdapter.FormHelper
 import com.chooongg.formAdapter.provider.FormTextProvider
 
-class MultiColumnForm internal constructor() : BaseForm(null, null) {
-
-    @IntRange(from = 1, to = 5)
-    var maxColumn = 2
+class SingleLineForm internal constructor() : BaseForm(null, null) {
 
     internal var items = mutableListOf<BaseForm>()
 
